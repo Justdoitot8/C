987dbd682cea8e4b0eb6979301a804f9e8af44b1
Justdoitot8 <167232407+Justdoitot8@users.noreply.github.com>
2024-09-07 12:59:45 +0300

    Add files via upload

---
+++ negativepozitive.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+	
+	int num=10;
+	
+	if(num>0){
+		printf("Possitive number.");
+	}else if(num<0){
+		printf("Negative number.");
+	}else{
+		printf("The value is 0");
+	}
+	return 0;
+	
+}
