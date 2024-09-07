337b5160d7e366a7cf6242c5b1b094f291844d20
Justdoitot8 <167232407+Justdoitot8@users.noreply.github.com>
2024-09-07 12:52:47 +0300

    Add files via upload

---
+++ code.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	
+	int doorCode=1337;
+	
+	if(doorCode==1337){
+		printf("Oper the door.");
+	} else{
+		printf("The code is wrong! You did not open the door.");
+	}
+	
+	return 0;
+	
+}
