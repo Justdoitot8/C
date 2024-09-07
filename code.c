9339eb41f3a52b5bc29ab36242ec87b5fb29943d
Justdoitot8 <167232407+Justdoitot8@users.noreply.github.com>
2024-09-07 13:19:01 +0300

    Update code.c

--- code.c
+++ code.c
@@ -5,7 +5,7 @@ int main(){
 	int doorCode=1337;
 	
 	if(doorCode==1337){
-		printf("Oper the door.");
+		printf("The door is now oper.");
 	} else{
 		printf("The code is wrong! You did not open the door.");
 	}
