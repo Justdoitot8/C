3358648cfa015aae0c0773403c4532487bb687c7
Justdoitot8 <167232407+Justdoitot8@users.noreply.github.com>
2024-09-07 20:42:07 +0300

    Add files via upload

---
+++ dddd.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+	
+	
+	int sayi;
+	
+	
+	printf("Sayi cift mi tek mi?\n Bir sayi giriniz:");
+	scanf("%d",&sayi);
+	
+	if(sayi%2==0){
+		
+		printf("Sayi cifttir.");
+		
+	}else{
+		
+		printf("Sayi tektir.");
+	}
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
