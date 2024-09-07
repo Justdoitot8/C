e103eae085db7f0a8d3b37182e1c995d7ffcb928
Justdoitot8 <167232407+Justdoitot8@users.noreply.github.com>
2024-09-07 13:07:52 +0300

    Update and rename deneme.c to votingAge.c

---
+++ votingAge.c
@@ -0,0 +1,16 @@
+
+#include <stdio.h>
+int main(){
+	
+	int myAge=25;
+	int votinAge=18;
+	
+	if(myAge>=votinAge){
+		printf("Old enough to vote.");
+	}else{
+		printf("Not old enough to vote.");
+	}
+	return 0;
+	
+	
+}
